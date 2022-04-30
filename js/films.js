b810d37a0e625323ed30ed0fcfac878fe234f330
jeremy-mls <contact@jeremy_monlouis.fr>
2022-04-30 19:20:52 +0200

    First commit

---
+++ js/films.js
@@ -0,0 +1,121 @@
+const programmation = [
+  {
+    date: "5 août",
+    films: [
+      {
+        titre: "Cette musique ne joue pour personne",
+        auteur: "Samuel Benchetrit",
+        description: `Dans une ville portuaire, des êtres isolés, habitués à la violence, vont soudain voir leurs vies bouleversées par le théâtre, la poésie et l'art. Leurs quotidiens vont également être transformés par l'amour`,
+        heure: "21h00",
+        img: "cette_musique_ne_joue_pour_personne.webp",
+        duration: "1h30"
+      },
+      {
+        titre: "Cette musique ne joue pour personne",
+        auteur: "Samuel Benchetrit",
+        description: `Dans une ville portuaire, des êtres isolés, habitués à la violence, vont soudain voir leurs vies bouleversées par le théâtre, la poésie et l'art. Leurs quotidiens vont également être transformés par l'amour`,
+        heure: "21h00",
+        img: "cette_musique_ne_joue_pour_personne.webp",
+        duration: "1h30"
+      },
+      {
+        titre: "Cette musique ne joue pour personne",
+        auteur: "Samuel Benchetrit",
+        description: `Dans une ville portuaire, des êtres isolés, habitués à la violence, vont soudain voir leurs vies bouleversées par le théâtre, la poésie et l'art. Leurs quotidiens vont également être transformés par l'amour`,
+        heure: "21h00",
+        img: "cette_musique_ne_joue_pour_personne.webp",
+        duration: "1h30"
+      },
+    ],
+  },
+  {
+    date: "6 août",
+    films: [
+      {
+        titre: "L'Autre",
+        auteur: "Charlotte Dauphin",
+        description:
+          "Marie est une jeune danseuse de l’Opéra de Paris. Après la mort brutale de son père le jour de son trentième anniversaire, elle décide d’arrêter sa pratique artistique et poursuit une existence de plus en plus recluse. Son deuil enferme Marie dans ses souvenirs, à travers les écrits mystérieux laissés par son père, ses pensées, et une maison qui semble l’emprisonner. Son amour pour Paul, un photographe qui a pris le dernier portrait de son père quelques minutes avant sa mort, va peu à peu éveiller en Marie une force vitale qui va l’aider à devenir une autre version d’elle-même.",
+        heure: "21h00",
+        img: "L_Autre.jpg",
+        duration: "1h30"
+      },
+      {
+        titre: "L'Autre",
+        auteur: "Charlotte Dauphin",
+        description:
+          "Marie est une jeune danseuse de l’Opéra de Paris. Après la mort brutale de son père le jour de son trentième anniversaire, elle décide d’arrêter sa pratique artistique et poursuit une existence de plus en plus recluse. Son deuil enferme Marie dans ses souvenirs, à travers les écrits mystérieux laissés par son père, ses pensées, et une maison qui semble l’emprisonner. Son amour pour Paul, un photographe qui a pris le dernier portrait de son père quelques minutes avant sa mort, va peu à peu éveiller en Marie une force vitale qui va l’aider à devenir une autre version d’elle-même.",
+        heure: "21h00",
+        img: "L_Autre.jpg",
+        duration: "1h30"
+      },
+      {
+        titre: "L'Autre",
+        auteur: "Charlotte Dauphin",
+        description:
+          "Marie est une jeune danseuse de l’Opéra de Paris. Après la mort brutale de son père le jour de son trentième anniversaire, elle décide d’arrêter sa pratique artistique et poursuit une existence de plus en plus recluse. Son deuil enferme Marie dans ses souvenirs, à travers les écrits mystérieux laissés par son père, ses pensées, et une maison qui semble l’emprisonner. Son amour pour Paul, un photographe qui a pris le dernier portrait de son père quelques minutes avant sa mort, va peu à peu éveiller en Marie une force vitale qui va l’aider à devenir une autre version d’elle-même.",
+        heure: "21h00",
+        img: "L_Autre.jpg",
+        duration: "1h30"
+      },
+    ],
+  },
+  {
+    date: "7 août",
+    films: [
+      {
+        titre: "",
+        auteur: "",
+        description: "",
+        heure: "",
+        img: "",
+        duration: "1h30"
+      },
+      {
+        titre: "La Cure",
+        auteur: "Clément Schneider et Simon Rembado",
+        description: "C’est l’été. Paul et sa femme Hélène, « les Parisiens », décident de profiter du récent déconfinement pour emmener Lisa, la jeune sœur de Paul en convalescence à la campagne. Là, ils retrouvent leurs amis d’enfance, Bruno et sa sœur Mélanie.",
+        heure: "21h00",
+        img: "La_Cure.jpg",
+        duration: "1h30"
+      },
+      {
+        titre: "",
+        auteur: "",
+        description: "",
+        heure: "",
+        img: "",
+        duration: "1h30"
+      },
+    ],
+  },
+  {
+    date: "8 août",
+    films: [
+      {
+        titre: "",
+        auteur: "",
+        description: "",
+        heure: "",
+        img: "",
+        duration: "1h30"
+      },
+      {
+        titre: "Messe Basse",
+        auteur: "Baptiste Drapeau",
+        description: "Dans une grande maison bourgeoise, deux femmes, une veuve et une étudiante, se disputent l’amour d’un homme disparu jusqu’à sombrer dans la folie.",
+        heure: "22h00",
+        img: "Messe_basse.jpg",
+        duration: "1h30"
+      },
+      {
+        titre: "",
+        auteur: "",
+        description: "",
+        heure: "",
+        img: "",
+        duration: "1h30"
+      },
+    ],
+  },
+];
