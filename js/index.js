dbe593150b95b4b06e14546a4c43d170f7b59770
jeremy-mls <contact@jeremy_monlouis.fr>
2022-05-07 20:03:39 +0200

    Cookie notice

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const cookieBox = document.getElementById('js-cookie-box');
+const cookieButton = document.getElementById('js-cookie-button');
+
+  
+  
+  cookieButton.onclick = function() {
+    cookieBox.classList.add('cookie-box--hide');
+  }
